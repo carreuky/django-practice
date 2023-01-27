ed7222323a3252ea297bc16622db708d6da189ad
carreuky <carreuky@gmail.com>
2023-01-27 16:39:58 +0300

    basic list,detail,update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 #now import the views.py file into this code
 from . import views
+from myapp.views import GeeksDetailView
+
 urlpatterns=[
-path('',views.index)
+path('',views.create_view),
+path('list/',views.list_view),
+path('<id>/', views.detail_view ),
+path('<id>/update/',views.update_view)
+
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,81 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import (render,get_object_or_404,HttpResponseRedirect)
+# relative import of forms
+from .models import GeeksModel
+from .forms import GeeksForm
+from django.views import View
+from django.views.generic.detail import DetailView
+
+
 
 
 def index(request):
 
-    return HttpResponse("Hello Geeks")
+    return HttpResponse("Hello Geeks gho")
+
+
+# relative import of forms
+
+def create_view(request):
+	# dictionary for initial data with
+	# field names as keys
+	context ={}
+
+	# add the dictionary during initialization
+	form = GeeksForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		
+	context['form']= form
+	return render(request, "create_view.html", context)
+
+def list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = GeeksModel.objects.all()
+         
+    return render(request, "list_view.html", context)
+
+def detail_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["data"] = GeeksModel.objects.get(id = id)
+         
+    return render(request, "detail_view.html", context)
+
+# update view for details
+def update_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # fetch the object related to passed id
+    obj = get_object_or_404(GeeksModel, id = id)
+ 
+    # pass the object as instance in form
+    form = GeeksForm(request.POST or None, instance = obj)
+ 
+    # save the data from the form and
+    # redirect to detail_view
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/"+id)
+ 
+    # add form dictionary to context
+    context["form"] = form
+ 
+    return render(request, "update_view.html", context)
+
+class GeeksDetailView(DetailView):
+    # specify the model to use
+    model = GeeksModel
+
+    
